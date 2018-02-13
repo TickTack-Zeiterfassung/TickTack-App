dcf9a63bc4268c11f956716e3531c8da91c96de0
Marcel Galster <marcel.galster@hotmail.com>
2018-02-14 00:46:21 +0100

    Report: Sliding List hinzugefügt.

--- src/pages/reports/report-project/report-project-page.ts
+++ src/pages/reports/report-project/report-project-page.ts
@@ -4,15 +4,32 @@ import { Project } from '../../../models/project.interface';
 
 @IonicPage()
 @Component({
-  selector: 'page-report-detail',
-  templateUrl: 'report-project-page.html',
+    selector: 'page-report-detail',
+    templateUrl: 'report-project-page.html',
 })
 export class ReportProjectPage {
     project = {} as Project;
     totalTime: string = '86 Stunden 3 Minuten 5 Sekunden';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    mockProjects = [
+        {
+            day: 'Montag, 20.11.2018',
+            stoppedTimes: [
+                { period: '08:00 Uhr - 09:34 Uhr', activity: 'Bild gemalt' },
+                { period: '10:00 Uhr - 11:20 Uhr' , activity: 'Projektmanagement' }
+            ]
+        },
+        {
+            day: 'Dienstag, 21.11.2018',
+            stoppedTimes: [
+                { period: '08:00 Uhr - 09:34 Uhr', activity: 'Bild gemalt' },
+                { period: '10:00 Uhr - 11:20 Uhr' , activity: 'Projektmanagement' }
+            ]
+        }
+    ];
+
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
 
     ionViewWillLoad(): void {
         if (this.navParams.get('project')) {
