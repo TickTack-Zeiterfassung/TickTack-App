999ceb66622cc1e7083b3fcefd7d3fea702e5cad
Matthias Fischer <matze6633@gmail.com>
2018-02-15 00:40:03 +0100

    TotalPipe an den Timer der CaptureTimePage gebunden

--- src/pipes/total-time/total-time.ts
+++ src/pipes/total-time/total-time.ts
@@ -2,15 +2,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'totalTimePipe'})
 export class TotalTimePipe implements PipeTransform {
-    transform(time: number): string {
+    transform(time: number, milli: boolean = true): string {
 
-        let tmp = Math.floor(time / 1000);
+        let tmp = 0;
+
+        if(milli) {
+            tmp = Math.floor(time / 1000);
+        } else {
+            tmp = time;
+        }
 
         const hours: number =  Math.floor(tmp / (60 * 60));
         tmp = tmp % (60 * 60);
         const mins: number = Math.floor(tmp / 60);
         const secs: number = tmp % 60;
 
-        return hours + ' Stunden ' + mins + ' Minuten ' + secs + ' Sekunden.';
+        return hours + ' Std. ' + mins + ' Min. ' + secs + ' Sek.';
     }
 }
